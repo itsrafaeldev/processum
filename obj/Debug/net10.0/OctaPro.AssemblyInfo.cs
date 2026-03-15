c7178f63b5c9e1873626b9cba6824955f63647e8
itsrafaeldev <almeida9137@gmail.com>
2026-03-15 11:01:06 -0300

    Novo gitignore

--- obj/Debug/net10.0/OctaPro.AssemblyInfo.cs
+++ obj/Debug/net10.0/OctaPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OctaPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6bc36e57bcd39df2c1fa149a88a1e667ba108ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bf6675514dd2f524177faad081647ed7b9aee6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OctaPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OctaPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
